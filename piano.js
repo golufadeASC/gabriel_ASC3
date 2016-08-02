1ee7ee81c057a3406c5088cc5bae552f1e24b695
golufadeASC <gabrielolufade@gmail.com>
2016-08-02 14:16:27 -0400

    piano game not finished but might continue for final project

---
+++ piano.js
@@ -0,0 +1,32 @@
+var value = 255;
+var value2=255;
+var i = 0;
+function setup(){
+    createCanvas(700,700);
+    
+}
+
+		
+
+var value = 255;
+function draw() {
+	if (keyIsDown(65))
+		value=0
+	fill(value,255,255)
+	
+	/*fill(value2,255,255);
+	rect(70,20,50,100);
+	rect(120,20,50,100);
+	rect(170,20,50,100);
+	rect(220,20,50,100);
+	rect(270,20,50,100);
+	rect(320,20,50,100);
+	rect(370,20,50,100);
+	rect(420,20,50,100);
+	rect(470,20,50,100);
+	rect(520,20,50,100);
+	rect(570,20,50,100);
+	rect(620,20,50,100);*/
+
+
+
